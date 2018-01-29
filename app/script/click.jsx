6984ee5c62063f5577ea3e3fb9b74bab8111088a
mitsuntu <t.mitsui000000@gmail.com>
2018-01-29 22:33:17 +0900

    180129 fix some code & depencency

--- app/script/click.jsx
+++
@@ -1,9 +0,0 @@
-import {ipcRenderer} from "electron";
-
-const myButton = document.getElementById("Click");
-
-let clickCount = 0;
-
-myButton.addEventListener("click", () => {
-  ipcRenderer.send("click-my-button", ++clickCount);
-});
--- app/script/react.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Main from '../view/view.jsx';
-
-ReactDOM.render(
-  <Main />,
-  document.getElementById('content')
-);
